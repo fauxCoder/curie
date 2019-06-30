036142dde4a34e1940ae0a0231fe6010c9ddd906
Jameson York <jamesonyork8@gmail.com>
2019-06-30 15:40:29 +1000

    No more 'shift'

--- SH.h
+++ SH.h
@@ -5,27 +5,26 @@
 
 struct SH
 {
-    SH(uint32_t a_t, uint32_t a_l, uint32_t a_shift = 0, double a_d = 0.0)
+    SH(uint32_t a_t, uint32_t a_l, double a_d = 0.0)
     : t(a_t)
     , l(a_l)
-    , shift(a_shift)
     , d(a_d)
     {
     }
 
     SH Sin(double f)
     {
-        return SH(t, l, shift, d + (std::sin((double)(t + shift) / f)));
+        return SH(t, l, d + (std::sin((double)(t / f))));
     }
 
     SH Saw(double f)
     {
-        return SH(t, l, shift, d + (t * f));
+        return SH(t, l, d + (t * f));
     }
 
     SH Scale(double s)
     {
-        return SH(t, l, shift, d * s);
+        return SH(t, l, d * s);
     }
 
     SH Cut(double c)
@@ -42,12 +41,7 @@ struct SH
             out = -level;
         }
 
-        return SH(t, l, shift, out);
-    }
-
-    SH Shift(uint32_t s)
-    {
-        return SH(t, l, shift + s, d);
+        return SH(t, l, out);
     }
 
     double Done()
@@ -57,6 +51,5 @@ struct SH
 
     uint32_t t;
     uint32_t l;
-    uint32_t shift;
     double d;
 };
